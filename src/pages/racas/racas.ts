69b955f23ceca51a8c6c8856660493bc0f02250e
Ezequias Moreira <ezequiasmoreira@hotmail.com>
2019-07-17 23:35:26 -0300

    Criado a lsitagem das raças

--- src/pages/racas/racas.ts
+++ src/pages/racas/racas.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RacaService } from '../../services/domain/raca.service';
+import { RacaDTO } from '../../models/raca.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the RacasPage page.
@@ -16,13 +18,17 @@ import { RacaService } from '../../services/domain/raca.service';
 })
 export class RacasPage {
 
+  ImageUrl: string = API_CONFIG.imageBaseUrl+"/racas";
+
+  items: RacaDTO[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public racaService: RacaService) {
   }
 
   ionViewDidLoad() {
     this.racaService.findAll()
       .subscribe(response => {
-        console.log(response);
+        this.items = response;
       },
       error => {
         console.log(error);
