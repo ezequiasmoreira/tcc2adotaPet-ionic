2ff37efc944e720573594046fcc655dfef91f491
Ezequias Moreira <ezequiasmoreira@hotmail.com>
2019-07-18 23:22:29 -0300

    implementando autenticação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { RacaService } from '../services/domain/raca.service';
 import { ErrorInterceptorProvider } from '../interceptors/ErrorInterceptor';
+import { AuthService } from '../services/auth.service';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { ErrorInterceptorProvider } from '../interceptors/ErrorInterceptor';
     SplashScreen,
     RacaService,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    ErrorInterceptorProvider
+    ErrorInterceptorProvider,
+    AuthService
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 import { MenuController } from 'ionic-angular/components/app/menu-controller';
 import { CredenciaisDTO } from '../../models/credenciais.dto';
+import { AuthService } from '../../services/auth.service';
 
 @IonicPage()
 @Component({
@@ -14,7 +15,7 @@ export class HomePage {
     email: "",
     senha: ""
   };
-  constructor(public navCtrl: NavController,public menu: MenuController) {
+  constructor(public navCtrl: NavController,public menu: MenuController,public auth: AuthService) {
 
   }
   ionViewWillEnter() {
@@ -26,8 +27,12 @@ export class HomePage {
   }
   login() {
     console.log(this.creds);
-    this.navCtrl.setRoot('RacasPage');
-  
+    this.auth.authenticate(this.creds)
+    .subscribe(response => {
+      console.log(response.headers.get('Authorization'));
+      this.navCtrl.setRoot('RacasPage');
+    },
+    error => {});   
   }
 
 }
