063987c0036bf4fd8aac71f0fc211e65041771ec
Ezequias Moreira <ezequiasmoreira@hotmail.com>
2019-07-19 23:26:39 -0300

    Implementando o refreshToken

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,6 +21,14 @@ export class HomePage {
   ionViewWillEnter() {
     this.menu.swipeEnable(false);
   }
+  ionViewDidEnter() {
+    this.auth.refreshToken()
+      .subscribe(response => {
+        this.auth.successfulLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('RacasPage');
+      },
+      error => {});  
+  }
     
   ionViewDidLeave() {
     this.menu.swipeEnable(true);
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -32,6 +32,15 @@ export class AuthService {
         };
         this.storage.setLocalUser(user);
     }
+    refreshToken() {
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/auth/refresh_token`, 
+            {},
+            {
+                observe: 'response',
+                responseType: 'text'
+            });
+    }
 
     logout() {
         this.storage.setLocalUser(null);
