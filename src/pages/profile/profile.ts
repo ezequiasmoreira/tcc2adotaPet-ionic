8cf2b0f59081c9787785056b389b59a0d0417c8e
Ezequias Moreira <ezequiasmoreira@hotmail.com>
2019-07-19 23:06:08 -0300

    Tratando erro 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -29,7 +29,13 @@ export class ProfilePage {
           this.pessoa = response;
           this.getImageIfExists();
         },
-        error => {});
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
+    }else{
+      this.navCtrl.setRoot('HomePage'); 
     }
   }
   getImageIfExists() {
